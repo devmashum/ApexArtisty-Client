75b0ca3be4c76cd6403bb4f9311c2ac985c5c310
devmashum <dev.mashum@gmail.com>
2023-11-26 23:46:15 +0100

    admin routes has been created for the security purpose

---
+++ src/Routes/AdminRoutes.jsx
@@ -0,0 +1,20 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../Hooks/useAdmin";
+import useAuth from "../Hooks/useAuth";
+
+
+const AdminRoutes = ({ children }) => {
+    const { user, loading } = useAuth();
+    const [isAdmin, isAdminLoading] = useAdmin();
+    const location = useLocation();
+    if (loading || isAdminLoading) {
+        return <progress className="progress progress-info w-56" value="100" max="100"></progress>
+    }
+    if (user && isAdmin) {
+        return children;
+    }
+
+    return <Navigate to={'/login'} state={{ from: location }} replace></Navigate>
+};
+
+export default AdminRoutes;
